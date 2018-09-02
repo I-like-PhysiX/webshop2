b50b98f79369e4e0f355f125b386057768d79773
I-like-PhysiX <32261812+I-like-PhysiX@users.noreply.github.com>
2018-09-02 21:11:18 +0200

    Update index.js

--- webshop2/src/router/index.js
+++ webshop2/src/router/index.js
@@ -1,15 +1,30 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import termekek from '@/components/termekek'
+import kosar from '@/components/kosar'
+import adatok from '@/components/adatok'
 
 Vue.use(Router)
 
 export default new Router({
+  mode:'history',
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
-})
+            {
+              path: '/',
+              component: termekek,
+              alias: '/kereses/:search/',
+              alias: '/:selected/:sortType/'
+            },
+           {
+              path: '/kosar',
+              name: 'kosar',
+              component: kosar
+           },
+           {
+              path: '/adatok',
+              name: 'adatok',
+              component: adatok
+           }
+         ]
+        }
+      )
