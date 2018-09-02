d96dcfdb8aa4181f7e03225cfe3c3233679fb96e
I-like-PhysiX <32261812+I-like-PhysiX@users.noreply.github.com>
2018-09-02 21:08:07 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+const fs = require('fs');
+let rawdata = fs.readFileSync('database.json');
+let adatok = JSON.parse(rawdata);
+app.use(express.static('static'))
+app.use(bodyParser.json())
+app.use((req, res, next) => {
+                              res.header('Access-Control-Allow-Origin','*')
+                              res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept')
+                              next()
+                            }
+       )
+app.get('/', (req, res) => res.send(adatok))
+app.listen(3000, () => console.log('Back-end szerver elérhető a http://localhost:3000 webcímen.'));
