7118623d0a9a066ed2a6faa62e200e37a2a74036
I-like-PhysiX <32261812+I-like-PhysiX@users.noreply.github.com>
2018-09-02 21:10:16 +0200

    Add files via upload

---
+++ webshop2/src/components/adatok.vue
@@ -0,0 +1,203 @@
+<template>
+  <div id ="adatok">
+    <div style="text-align:center; margin: 15px auto;">Adatok megadása és vásárlás</div>
+      <div class="main">
+        <b-modal ref="myModalRef" hide-footer title="Bevásárló alkalmazás">
+          <div class="d-block text-center">
+            <h3>Köszönjük a vásárlást!</h3>
+          </div>
+          <b-btn
+                 class="mt-3"
+                 variant="outline-danger"
+                 block
+                 v-on:click="hideModal">Bezárás</b-btn>
+        </b-modal>
+    <div class="container">
+      <b-form @submit="onSubmit" @reset="onReset">
+        <b-form-group id="exampleInputGroup1"
+                      label="Név: *"
+                      label-for="exampleInput1">
+          <b-form-input id="exampleInput1"
+                        type="text"
+                        v-model="input1"
+                        required
+                        placeholder="Név">
+          </b-form-input>
+        </b-form-group>
+        <b-form-group id="exampleInputGroup2"
+                      label="Cím: *"
+                      label-for="exampleInput2">
+          <b-form-input id="exampleInput2"
+                        type="text"
+                        v-model="input2"
+                        required
+                        placeholder="Cím">
+          </b-form-input>
+        </b-form-group>
+        <b-form-group id="exampleInputGroup3"
+                      label="Telefonszám: *"
+                      label-for="exampleInput3">
+          <b-form-input id="exampleInput3"
+                        type="tel"
+                        v-model="input3"
+                        required
+                        placeholder="0630123456">
+          </b-form-input>
+        </b-form-group>
+        <b-form-group id="exampleInputGroup4"
+                      label="Email cím: *"
+                      label-for="exampleInput4">
+          <b-form-input id="exampleInput4"
+                        type="email"
+                        v-model="input4"
+                        required
+                        placeholder="példa@email.com">
+          </b-form-input>
+        </b-form-group>
+        <b-form-group id="exampleGroup5">
+          <b-form-checkbox-group v-model="input5" id="exampleChecks" required>
+            <p><b-form-checkbox value="GDPR">Egyetértek a <a href="https://www.eugdpr.org/" target="_blank">GDPR</a> feltételeivel. *</b-form-checkbox></p>
+            <p><b-form-checkbox value="ÁSZF">Egyetértek az <a href="http://www.kormany.hu/download/b/db/21000/20170911%20IM%20%C3%81SZF%20felhaszn%C3%A1l%C3%A1si%20felt%C3%A9telek_felhaszn%C3%A1l%C3%A1si%20szerz%C5%91d%C3%A9sek.pdf" target="_blank">ÁSZF</a> feltételeivel. *</b-form-checkbox></p>
+          </b-form-checkbox-group>
+        </b-form-group>
+        <br>
+        <br>
+        <b-button-group class="responsive">
+          <b-btn v-on:click="basketrouter()" size="sm" variant="primary">Kosár</b-btn>
+          <b-button type="reset" size="sm" variant="danger">Tötlés</b-button>
+          <b-button type="submit" size="sm" variant="success">Vásárlás</b-button>
+        </b-button-group>
+      </b-form>
+      <br>
+      <br>
+    </div>
+</div>
+</div>
+</template>
+
+<script>
+
+import {mapGetters} from 'vuex';
+
+export default {
+  name: 'adatok',
+  data() {
+    return {
+
+    }
+  },
+
+  watch: {
+
+   },
+
+  methods: {
+    onReset (event) {
+      event.preventDefault();
+      this.$store.commit('onReset', event);
+    },
+    onSubmit (event) {
+      event.preventDefault();
+      console.log('Make API request');
+      this.showModal();
+    },
+    showModal () {
+      this.$refs.myModalRef.show();
+    },
+    hideModal () {
+      this.$refs.myModalRef.hide();
+      this.$store.commit('onClose');
+      this.$store.commit('init');
+    },
+    basketrouter(){
+      this.$store.commit('basketrouter');
+    }
+  },
+
+  mounted() {
+    console.log("adatok init");
+  },
+
+  computed: {
+    input1:{
+        get(){ return this.$store.getters.input1; },
+        set( value ){ this.$store.commit("input1", value );}
+    },
+    input2:{
+        get(){ return this.$store.getters.input2; },
+        set( value ){ this.$store.commit("input2", value );}
+    },
+    input3:{
+        get(){ return this.$store.getters.input3; },
+        set( value ){ this.$store.commit("input3", value );}
+    },
+    input4:{
+        get(){ return this.$store.getters.input4; },
+        set( value ){ this.$store.commit("input4", value );}
+    },
+    input5:{
+        get(){ return this.$store.getters.input5; },
+        set( value ){ this.$store.commit("input5", value );}
+    }
+  }
+}
+</script>
+
+<style scoped>
+ #adatok{
+  font-family: courier;
+  box-sizing: border-box;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-orient: vertical;
+    -webkit-box-direction: normal;
+    -webkit-flex-direction: column;
+    -ms-flex-direction: column;
+    flex-direction: column;
+    min-height: 100vh;
+ }
+ .main{
+  width:100%;
+  overflow:hidden;
+}
+.bottom,.text-mid {
+  padding:20px 10px;
+  height:100%;
+}
+.text-mid {
+  margin-top: auto;
+  background:white;
+}
+ .form-control {
+    display: inline-block;
+}
+#card:hover{
+  border-width:1px;
+  border-style:ridge;
+  border-color:#4285F4;
+}
+.exPopoverReactive1:hover{
+  border-width:1px;
+  border-style:ridge;
+  border-color:#FFFFFF;
+}
+.image {
+    position: relative;
+}
+.top-right {
+    position: absolute;
+    top: 8px;
+    right: 16px;
+}
+table {
+    border-collapse: collapse;
+    width: 100%;
+}
+th, td {
+    padding: 8px;
+    text-align: left;
+    border-bottom: 1px solid #ddd;
+}
+</style>
---
+++ webshop2/src/components/kosar.vue
@@ -0,0 +1,156 @@
+<template>
+  <div id ="kosar">
+    <div style="text-align:center; margin: 15px auto;">Kosár</div>
+      <div class="main">
+        <div class="container">
+          <b-alert show class="small" style="background-color: #F5F5F5;">
+            <h3 style="color: red; text-align: center; margin: 45px 0px;" v-if="rendeles==0">A kosár üres!</h3>
+            <table v-else>
+              <thead>
+                <tr>
+                  <td><strong>Termék neve</strong></td>
+                  <td><strong>Egységár</strong></td>
+                  <td><strong>Mennyiség</strong></td>
+                  <td><strong>Részösszeg</strong></td>
+                </tr>
+              </thead>
+              <tbody>
+               <tr v-for="(elem, index) in rendeles" :key="elem.id">
+                  <td>{{ elem.termek }}</td>
+                  <td>{{ elem.egysar }}/{{ elem.egys }}</td>
+                  <td>
+                   <div
+                           v-if="elem.egys=='kg'"
+                           v-model="elem.alap">
+                           <b-button v-on:click="removefromcart(elem, 0.1)">-</b-button>
+                           <b-button :disabled="elem.alap == elem.ossz ? true : false" v-on:click="addtocart(elem, 0.1)">+</b-button>
+                           {{elem.alap}} {{elem.egys}}
+                    </div>
+                    <div
+                           v-if="elem.egys!='kg'"
+                           v-model="elem.alap">
+                           <b-button v-on:click="removefromcart(elem, 1)">-</b-button>
+                           <b-button :disabled="elem.alap == elem.ossz ? true : false" v-on:click="addtocart(elem, 1)">+</b-button>
+                           {{elem.alap}} {{elem.egys}}
+                    </div>
+                  </td>
+                  <td>
+                    {{Math.round(elem.alap*elem.egysar)}} Ft
+                    <b-button v-on:click="del(elem)">X</b-button>
+                  </td>
+               </tr>
+              </tbody>
+            </table>
+          </b-alert>
+          <br>
+          <br>
+          <strong>Összesen fizetendő:</strong> <b>{{osszeg}} Ft</b>
+          <br>
+          <br>
+          <b-button-group class="responsive">
+           <b-btn v-on:click="onClose()" size="sm" variant="danger" :disabled="rendeles==0 ? true : false">Kosár kiürítése</b-btn>
+           <b-btn v-on:click="datarouter()" size="sm" variant="success" :disabled="rendeles==0 ? true : false">Adatok</b-btn>
+        </b-button-group>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import {mapGetters} from 'vuex';
+
+export default {
+  name: 'kosar',
+  data() {
+    return {
+
+    }
+  },
+  watch: {
+
+   },
+  methods: {
+    removefromcart(elem, step){
+      this.$store.commit('removefromcart', elem, step);
+    },
+    addtocart(elem, step){
+      this.$store.commit('addtocart', elem, step);
+    },
+    del(elem){
+      this.$store.commit('del', elem);
+    },
+    onClose(){
+      this.$store.commit('onClose');
+    },
+    datarouter(){
+      this.$store.commit('datarouter');
+    }
+  },
+  mounted() {
+    console.log('kosar init');
+  },
+  computed: {
+    ...mapGetters(['rendeles','osszeg'])
+  }
+}
+</script>
+
+<style scoped>
+ #kosar{
+  font-family: courier;
+  box-sizing: border-box;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-orient: vertical;
+    -webkit-box-direction: normal;
+    -webkit-flex-direction: column;
+    -ms-flex-direction: column;
+    flex-direction: column;
+    min-height: 100vh;
+ }
+ .main{
+  width:100%;
+  overflow:hidden;
+}
+.bottom,.text-mid {
+  padding:20px 10px;
+  height:100%;
+}
+.text-mid {
+  margin-top: auto;
+  background:white;
+}
+ .form-control {
+    display: inline-block;
+}
+#card:hover{
+  border-width:1px;
+  border-style:ridge;
+  border-color:#4285F4;
+}
+.exPopoverReactive1:hover{
+  border-width:1px;
+  border-style:ridge;
+  border-color:#FFFFFF;
+}
+.image {
+    position: relative;
+}
+.top-right {
+    position: absolute;
+    top: 8px;
+    right: 16px;
+}
+table {
+    border-collapse: collapse;
+    width: 100%;
+}
+th, td {
+    padding: 8px;
+    text-align: left;
+    border-bottom: 1px solid #ddd;
+}
+</style>
