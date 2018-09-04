ddc1e91aa3ebffc049f97809eda9c62a72f9729a
I-like-PhysiX <32261812+I-like-PhysiX@users.noreply.github.com>
2018-09-04 07:04:09 +0200

    Update termekek.vue

--- webshop2/src/components/termekek.vue
+++ webshop2/src/components/termekek.vue
@@ -1,52 +1,46 @@
 <template>
- <div id="termekek">
-  <div style="text-align:center; margin: 15px auto;">Elérhető termékek</div>
-   <div class="main" v-if="szurttombhossz!=0">
-    <div class="container">
-     <div class="row" v-for="i in Math.ceil(szurttombhossz / itemsPerRow)">
-      <b-card
-              bg-variant="light"
-              id="card"
-              footer-bg-variant="secondary"
-              v-for="(elem, index) in paginatedData.slice((i - 1) * itemsPerRow, i * itemsPerRow)"
-              :key="elem.id"
-              :img-src="elem.url"
-              v-bind:style= "[!elem.raktaron ? {opacity: 0.6} : {}]"
-              style="max-width: 274px;"
-              img-alt="A termék képe"
-              img-top
-              class="image">
-       <div class="top-right"><h3 style="color: red; background-color: yellow;"><strong>{{elem.info}}</strong></h3></div>
-       <p class="card-text"><p>{{elem.termek}}</p> <p>{{elem.egysar}} Ft/{{elem.egys}}</p></p>
-       <p class="card-text" style="color: green;" v-if="elem.raktaron">Raktáron</p>
-       <p class="card-text" style="color: red;" v-else>Elfogyott</p>
-       <div
-            v-if="elem.egys=='kg'"
-            v-model="elem.alap">
-            <b-button :disabled="!elem.raktaron" v-on:click="addtocart(elem, 0.1)">Kosárba</b-button>
-       </div>
-       <div
-            v-if="elem.egys!='kg'"
-            v-model="elem.alap">
-            <b-button :disabled="!elem.raktaron" v-on:click="addtocart(elem, 1)">Kosárba</b-button>
-       </div>
-      </b-card>
-     </div>
-    </div>
-   </div>
-   <div class="text-mid">
-     <div class="center" style="text-align: center;">
-       <b-button :disabled="pageNumber == 0 ? true : false" class="pagination-btn" v-on:click="updatepageNumber(-1)"> < </b-button>
-       {{pageNumber}}/{{pageCount}}
-       <b-button :disabled="pageNumber == pageCount ? true : false" class="pagination-btn" v-on:click="updatepageNumber(1)"> > </b-button>
-     </div>
-   </div>
- </div>
+  <div id="termekek">
+    <div style="text-align:center; margin: 15px auto;">Elérhető termékek</div>
+    <div class="main" v-if="szurttombhossz!=0">
+      <div class="container">
+        <div class="row" v-for="i in Math.ceil(szurttombhossz / itemsPerRow)">
+          <b-card
+          bg-variant="light"
+          id="card"
+          footer-bg-variant="secondary"
+          v-for="(elem, index) in paginatedData.slice((i - 1) * itemsPerRow, i * itemsPerRow)"
+          :key="elem.id"
+          :img-src="elem.url"
+          v-bind:style= "[!elem.raktaron ? {opacity: 0.6} : {}]"
+          style="max-width: 274px;"
+          img-alt="A termék képe"
+          img-top
+          class="image">
+          <div class="top-right"><h3 style="color: red; background-color: yellow;"><strong>{{elem.info}}</strong></h3></div>
+          <p class="card-text"><p>{{elem.termek}}</p> <p>{{elem.egysar}} Ft/{{elem.egys}}</p></p>
+          <p class="card-text" style="color: green;" v-if="elem.raktaron">Raktáron</p>
+          <p class="card-text" style="color: red;" v-else>Elfogyott</p>
+          <div
+          v-if="elem.egys=='kg'"
+          v-model="elem.alap">
+          <b-button :disabled="!elem.raktaron" v-on:click="addtocart(elem, 0.1)">Kosárba</b-button>
+        </div>
+        <div
+        v-if="elem.egys!='kg'"
+        v-model="elem.alap">
+        <b-button :disabled="!elem.raktaron" v-on:click="addtocart(elem, 1)">Kosárba</b-button>
+      </div>
+    </b-card>
+  </div>
+</div>
+</div>
+</div>
 </template>
 
 <script>
 
 import {mapGetters} from 'vuex';
+import { mapMutations } from 'vuex';
 
 export default {
   name: 'termekek',
@@ -55,72 +49,66 @@ export default {
     }
   },
   watch: {
-
-   },
+  },
   methods: {
-    addtocart(elem, step){
-      this.$store.commit('addtocart', elem, step);
-    },
-    updatepageNumber(diff){
-      this.$store.commit('updatepageNumber', diff);
-    }
+    ...mapMutations(['addtocart'])
   },
   mounted() {
     console.log("termekek init");
   },
   computed: {
-    ...mapGetters(['pageCount','pageNumber','paginatedData','itemsPerRow','szurttombhossz'])
+    ...mapGetters(['paginatedData','itemsPerRow','szurttombhossz'])
   }
 }
 </script>
 
 <style scoped>
 #termekek{
- font-family: courier;
- box-sizing: border-box;
-   display: -webkit-box;
-   display: -webkit-flex;
-   display: -ms-flexbox;
-   display: flex;
-   -webkit-box-orient: vertical;
-   -webkit-box-direction: normal;
-   -webkit-flex-direction: column;
-   -ms-flex-direction: column;
-   flex-direction: column;
-   margin: 0 auto;
-   min-height: 100vh;
+  font-family: courier;
+  box-sizing: border-box;
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  -webkit-flex-direction: column;
+  -ms-flex-direction: column;
+  flex-direction: column;
+  margin: 0 auto;
+  min-height: 100vh;
 }
 .main{
- width:100%;
- overflow:hidden;
+  width:100%;
+  overflow:hidden;
 }
 .bottom,.text-mid {
- padding:20px 10px;
- height:100%;
+  padding:20px 10px;
+  height:100%;
 }
 .text-mid {
- margin-top: auto;
- background:white;
+  margin-top: auto;
+  background:white;
 }
 .form-control {
-   display: inline-block;
+  display: inline-block;
 }
 #card:hover{
- border-width:1px;
- border-style:ridge;
- border-color:#4285F4;
+  border-width:1px;
+  border-style:ridge;
+  border-color:#4285F4;
 }
 .exPopoverReactive1:hover{
- border-width:1px;
- border-style:ridge;
- border-color:#FFFFFF;
+  border-width:1px;
+  border-style:ridge;
+  border-color:#FFFFFF;
 }
 .image {
-   position: relative;
+  position: relative;
 }
 .top-right {
-   position: absolute;
-   top: 8px;
-   right: 16px;
+  position: absolute;
+  top: 8px;
+  right: 16px;
 }
 </style>
