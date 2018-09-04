df38ebdb5eac8f2df906ef13e985c04461f8360a
I-like-PhysiX <32261812+I-like-PhysiX@users.noreply.github.com>
2018-09-04 07:05:09 +0200

    Update adatok.vue

--- webshop2/src/components/adatok.vue
+++ webshop2/src/components/adatok.vue
@@ -1,76 +1,76 @@
 <template>
   <div id ="adatok">
     <div style="text-align:center; margin: 15px auto;">Adatok megadása és vásárlás</div>
-      <div class="main">
-        <b-modal ref="myModalRef" hide-footer title="Bevásárló alkalmazás">
-          <div class="d-block text-center">
-            <h3>Köszönjük a vásárlást!</h3>
-          </div>
-          <b-btn
-                 class="mt-3"
-                 variant="outline-danger"
-                 block
-                 v-on:click="hideModal">Bezárás</b-btn>
-        </b-modal>
-    <div class="container">
-      <b-form @submit="onSubmit" @reset="onReset">
-        <b-form-group id="exampleInputGroup1"
-                      label="Név: *"
-                      label-for="exampleInput1">
+    <div class="main">
+      <b-modal ref="myModalRef" hide-footer title="Bevásárló alkalmazás">
+        <div class="d-block text-center">
+          <h3>Köszönjük a vásárlást!</h3>
+        </div>
+        <b-btn
+        class="mt-3"
+        variant="outline-danger"
+        block
+        v-on:click="hideModal">Bezárás</b-btn>
+      </b-modal>
+      <div class="container">
+        <b-form @submit="onSubmit" @reset="onReset">
+          <b-form-group id="exampleInputGroup1"
+          label="Név: *"
+          label-for="exampleInput1">
           <b-form-input id="exampleInput1"
-                        type="text"
-                        v-model="input1"
-                        required
-                        placeholder="Név">
-          </b-form-input>
-        </b-form-group>
-        <b-form-group id="exampleInputGroup2"
-                      label="Cím: *"
-                      label-for="exampleInput2">
-          <b-form-input id="exampleInput2"
-                        type="text"
-                        v-model="input2"
-                        required
-                        placeholder="Cím">
-          </b-form-input>
-        </b-form-group>
-        <b-form-group id="exampleInputGroup3"
-                      label="Telefonszám: *"
-                      label-for="exampleInput3">
-          <b-form-input id="exampleInput3"
-                        type="tel"
-                        v-model="input3"
-                        required
-                        placeholder="0630123456">
-          </b-form-input>
-        </b-form-group>
-        <b-form-group id="exampleInputGroup4"
-                      label="Email cím: *"
-                      label-for="exampleInput4">
-          <b-form-input id="exampleInput4"
-                        type="email"
-                        v-model="input4"
-                        required
-                        placeholder="példa@email.com">
-          </b-form-input>
-        </b-form-group>
-        <b-form-group id="exampleGroup5">
-          <b-form-checkbox-group v-model="input5" id="exampleChecks" required>
-            <p><b-form-checkbox value="GDPR">Egyetértek a <a href="https://www.eugdpr.org/" target="_blank">GDPR</a> feltételeivel. *</b-form-checkbox></p>
-            <p><b-form-checkbox value="ÁSZF">Egyetértek az <a href="http://www.kormany.hu/download/b/db/21000/20170911%20IM%20%C3%81SZF%20felhaszn%C3%A1l%C3%A1si%20felt%C3%A9telek_felhaszn%C3%A1l%C3%A1si%20szerz%C5%91d%C3%A9sek.pdf" target="_blank">ÁSZF</a> feltételeivel. *</b-form-checkbox></p>
-          </b-form-checkbox-group>
-        </b-form-group>
-        <br>
-        <br>
-        <b-button-group class="responsive">
-          <b-btn v-on:click="basketrouter()" size="sm" variant="primary">Kosár</b-btn>
-          <b-button type="reset" size="sm" variant="danger">Tötlés</b-button>
-          <b-button type="submit" size="sm" variant="success">Vásárlás</b-button>
-        </b-button-group>
-      </b-form>
-      <br>
-      <br>
-    </div>
+          type="text"
+          v-model="input1"
+          required
+          placeholder="Név">
+        </b-form-input>
+      </b-form-group>
+      <b-form-group id="exampleInputGroup2"
+      label="Cím: *"
+      label-for="exampleInput2">
+      <b-form-input id="exampleInput2"
+      type="text"
+      v-model="input2"
+      required
+      placeholder="Cím">
+    </b-form-input>
+  </b-form-group>
+  <b-form-group id="exampleInputGroup3"
+  label="Telefonszám: *"
+  label-for="exampleInput3">
+  <b-form-input id="exampleInput3"
+  type="tel"
+  v-model="input3"
+  required
+  placeholder="0630123456">
+</b-form-input>
+</b-form-group>
+<b-form-group id="exampleInputGroup4"
+label="Email cím: *"
+label-for="exampleInput4">
+<b-form-input id="exampleInput4"
+type="email"
+v-model="input4"
+required
+placeholder="példa@email.com">
+</b-form-input>
+</b-form-group>
+<b-form-group id="exampleGroup5">
+  <b-form-checkbox-group v-model="input5" id="exampleChecks" required>
+    <p><b-form-checkbox value="GDPR">Egyetértek a <a href="https://www.eugdpr.org/" target="_blank">GDPR</a> feltételeivel. *</b-form-checkbox></p>
+    <p><b-form-checkbox value="ÁSZF">Egyetértek az <a href="http://www.kormany.hu/download/b/db/21000/20170911%20IM%20%C3%81SZF%20felhaszn%C3%A1l%C3%A1si%20felt%C3%A9telek_felhaszn%C3%A1l%C3%A1si%20szerz%C5%91d%C3%A9sek.pdf" target="_blank">ÁSZF</a> feltételeivel. *</b-form-checkbox></p>
+  </b-form-checkbox-group>
+</b-form-group>
+<br>
+<br>
+<b-button-group class="responsive">
+  <b-btn v-on:click="basketrouter()" size="sm" variant="primary">Kosár</b-btn>
+  <b-button type="reset" size="sm" variant="danger">Tötlés</b-button>
+  <b-button type="submit" size="sm" variant="success">Vásárlás</b-button>
+</b-button-group>
+</b-form>
+<br>
+<br>
+</div>
 </div>
 </div>
 </template>
@@ -78,6 +78,7 @@
 <script>
 
 import {mapGetters} from 'vuex';
+import { mapMutations } from 'vuex';
 
 export default {
   name: 'adatok',
@@ -89,9 +90,10 @@ export default {
 
   watch: {
 
-   },
+  },
 
   methods: {
+    ...mapMutations(['basketrouter']),
     onReset (event) {
       event.preventDefault();
       this.$store.commit('onReset', event);
@@ -108,9 +110,6 @@ export default {
       this.$refs.myModalRef.hide();
       this.$store.commit('onClose');
       this.$store.commit('init');
-    },
-    basketrouter(){
-      this.$store.commit('basketrouter');
     }
   },
 
@@ -120,45 +119,45 @@ export default {
 
   computed: {
     input1:{
-        get(){ return this.$store.getters.input1; },
-        set( value ){ this.$store.commit("input1", value );}
+      get(){ return this.$store.getters.input1; },
+      set( value ){ this.$store.commit("input1", value );}
     },
     input2:{
-        get(){ return this.$store.getters.input2; },
-        set( value ){ this.$store.commit("input2", value );}
+      get(){ return this.$store.getters.input2; },
+      set( value ){ this.$store.commit("input2", value );}
     },
     input3:{
-        get(){ return this.$store.getters.input3; },
-        set( value ){ this.$store.commit("input3", value );}
+      get(){ return this.$store.getters.input3; },
+      set( value ){ this.$store.commit("input3", value );}
     },
     input4:{
-        get(){ return this.$store.getters.input4; },
-        set( value ){ this.$store.commit("input4", value );}
+      get(){ return this.$store.getters.input4; },
+      set( value ){ this.$store.commit("input4", value );}
     },
     input5:{
-        get(){ return this.$store.getters.input5; },
-        set( value ){ this.$store.commit("input5", value );}
+      get(){ return this.$store.getters.input5; },
+      set( value ){ this.$store.commit("input5", value );}
     }
   }
 }
 </script>
 
 <style scoped>
- #adatok{
+#adatok{
   font-family: courier;
   box-sizing: border-box;
-    display: -webkit-box;
-    display: -webkit-flex;
-    display: -ms-flexbox;
-    display: flex;
-    -webkit-box-orient: vertical;
-    -webkit-box-direction: normal;
-    -webkit-flex-direction: column;
-    -ms-flex-direction: column;
-    flex-direction: column;
-    min-height: 100vh;
- }
- .main{
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  -webkit-flex-direction: column;
+  -ms-flex-direction: column;
+  flex-direction: column;
+  min-height: 100vh;
+}
+.main{
   width:100%;
   overflow:hidden;
 }
@@ -170,8 +169,8 @@ export default {
   margin-top: auto;
   background:white;
 }
- .form-control {
-    display: inline-block;
+.form-control {
+  display: inline-block;
 }
 #card:hover{
   border-width:1px;
@@ -184,20 +183,20 @@ export default {
   border-color:#FFFFFF;
 }
 .image {
-    position: relative;
+  position: relative;
 }
 .top-right {
-    position: absolute;
-    top: 8px;
-    right: 16px;
+  position: absolute;
+  top: 8px;
+  right: 16px;
 }
 table {
-    border-collapse: collapse;
-    width: 100%;
+  border-collapse: collapse;
+  width: 100%;
 }
 th, td {
-    padding: 8px;
-    text-align: left;
-    border-bottom: 1px solid #ddd;
+  padding: 8px;
+  text-align: left;
+  border-bottom: 1px solid #ddd;
 }
 </style>
