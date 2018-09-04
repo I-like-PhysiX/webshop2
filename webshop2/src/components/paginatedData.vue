86399db9160f0ff946ea11dcf6690d48f0fdb90d
I-like-PhysiX <32261812+I-like-PhysiX@users.noreply.github.com>
2018-09-04 07:06:20 +0200

    Add files via upload

---
+++ webshop2/src/components/paginatedData.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="paginatedData" class="text-mid">
+    <div class="center" style="text-align: center;">
+      <b-button :disabled="pageNumber == 0 ? true : false" class="pagination-btn" v-on:click="updatepageNumber(-1)"> < </b-button>
+      {{pageNumber}}/{{pageCount}}
+      <b-button :disabled="pageNumber == pageCount ? true : false" class="pagination-btn" v-on:click="updatepageNumber(1)"> > </b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import {mapGetters} from 'vuex';
+import { mapMutations } from 'vuex';
+
+export default {
+
+  data() {
+    return {
+
+    }
+  },
+  watch: {
+
+  },
+  methods: {
+    ...mapMutations(['updatepageNumber'])
+  },
+  mounted() {
+    console.log("paginatedData init");
+  },
+  computed: {
+    ...mapGetters(['pageCount','pageNumber']),
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
