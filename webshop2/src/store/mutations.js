a41ac830d1b09f6cb07e632c4d3e08c044e6bdd1
I-like-PhysiX <32261812+I-like-PhysiX@users.noreply.github.com>
2018-09-04 07:00:49 +0200

    Update mutations.js

--- webshop2/src/store/mutations.js
+++ webshop2/src/store/mutations.js
@@ -2,11 +2,9 @@ export default {
   updatepageNumber(state, diff){
     state.pageNumber+=diff;
   },
-  /*
   tomb(state, adat){
     state.tomb=adat;
   },
-  */
   addtocart(state, elem, step){
     elem.alap = Math.round((elem.alap + step) * 10) / 10;
     state.rendeles.push(elem);
@@ -36,97 +34,93 @@ export default {
     let mySet = new Set();
     state.tomb.forEach(v => mySet.add(v.type));
     let myArray = Array.from(mySet);
+    state.species=[];
     myArray.forEach(v => state.species.push({
-                                        text: v,
-                                        value: v,
-                                        isdisabled: false
-                                     })
-    );
-  },
-  onClose() {
-    state.rendeles=[];
-    console.log('Reset cart');
-  },
-  initrouter(state){
-    state.url=`/`;
-    this.$router.push(state.url);
-    state.csaktermekek=true;
-  },
-  searchrouter(state){
-    state.url=`/kereses/${state.search}/`;
-    this.$router.push(state.url);
-    state.csaktermekek=true;
-    state.search='';
-  },
-  categoryrouter(state){
-    state.url=`/${state.selected}/${state.sortType+"_szerinti_rendezes"}/`;
-    this.$router.push(state.url);
-    state.csaktermekek=true;
-  },
-  basketrouter(state){
-    state.url=`/kosar`;
-    this.$router.push(state.url);
-    state.search='';
-    state.csaktermekek=false;
-  },
-  datarouter(state){
-    state.url=`/adatok`;
-    this.$router.push(state.url);
-    state.csaktermekek=false;
-  },
-  szur(state) {
-    state.pageNumber=0;
-    state.szurttomb = state.tomb.filter(v => v.type==state.selected);
-    switch (state.sortType) {
-        case 'termek':
-        state.szurttomb=state.szurttomb.sort((a,b) => a.termek > b.termek);
-            break;
-        case 'egysar':
-        state.szurttomb=state.szurttomb.sort((a,b) => a.egysar > b.egysar);
-            break;
-          };
-      state.itemsPerRow=state.szurttomb.length;
-      state.szurttombhossz=state.szurttomb.length;
-      categoryrouter();
-    },
-    szur2(state){
-      state.pageNumber=0;
-      if (state.search!='') {
-        state.szurttomb =state.tomb.filter(v => RegExp(state.search,'i').test(v.termek)).slice(0,9);
-        state.itemsPerRow=state.szurttomb.length;
-        state.szurttombhossz=state.szurttomb.length;
-        searchrouter(state);
-      }
-    },
-    selected(state, payload){
-      state.selected=payload;
-    },
-    sortType(state, payload){
-      state.sortType=payload;
-    },
-    search(state, payload){
-      state.search=payload;
-    },
-    input1(state, payload){
-      state.input1=payload;
-    },
-    input2(state, payload){
-      state.input2=payload;
-    },
-    input3(state, payload){
-      state.input3=payload;
-    },
-    input4(state, payload){
-      state.input4=payload;
-    },
-    input5(state, payload){
-      state.input5=payload;
-    },
-    onReset(state, payload){
-      state.input1='';
-      state.input2='';
-      state.input3='';
-      state.input4='';
-      state.input5='';
-    }
+      text: v,
+      value: v,
+      isdisabled: false
+    })
+  );
+},
+onClose() {
+  state.rendeles=[];
+  console.log('Reset cart');
+},
+initrouter(state){
+  this.$router.push(`/`);
+  state.csaktermekek=true;
+},
+searchrouter(state){
+  this.$router.push(`/kereses/${state.search}/`);
+  state.csaktermekek=true;
+  state.search='';
+},
+categoryrouter(state){
+  this.$router.push(`/${state.selected}/${state.sortType+"_szerinti_rendezes"}/`);
+  state.csaktermekek=true;
+},
+basketrouter(state){
+  this.$router.push(`/kosar/`);
+  state.search='';
+  state.csaktermekek=false;
+},
+datarouter(state){
+  this.$router.push(`/adatok/`);
+  state.csaktermekek=false;
+},
+szur(state) {
+  state.pageNumber=0;
+  state.szurttomb = state.tomb.filter(v => v.type==state.selected);
+  switch (state.sortType) {
+    case 'termek':
+    state.szurttomb=state.szurttomb.sort((a,b) => a.termek > b.termek);
+    break;
+    case 'egysar':
+    state.szurttomb=state.szurttomb.sort((a,b) => a.egysar > b.egysar);
+    break;
+  };
+  state.itemsPerRow=state.szurttomb.length;
+  state.szurttombhossz=state.szurttomb.length;
+  categoryrouter();
+},
+szur2(state){
+  state.pageNumber=0;
+  if (state.search!='') {
+    state.szurttomb =state.tomb.filter(v => RegExp(state.search,'i').test(v.termek)).slice(0,9);
+    state.itemsPerRow=state.szurttomb.length;
+    state.szurttombhossz=state.szurttomb.length;
+    searchrouter(state);
+  }
+},
+selected(state, payload){
+  state.selected=payload;
+},
+sortType(state, payload){
+  state.sortType=payload;
+},
+search(state, payload){
+  state.search=payload;
+},
+input1(state, payload){
+  state.input1=payload;
+},
+input2(state, payload){
+  state.input2=payload;
+},
+input3(state, payload){
+  state.input3=payload;
+},
+input4(state, payload){
+  state.input4=payload;
+},
+input5(state, payload){
+  state.input5=payload;
+},
+onReset(state, payload){
+  state.input1='';
+  state.input2='';
+  state.input3='';
+  state.input4='';
+  state.input5='';
+}
 }
